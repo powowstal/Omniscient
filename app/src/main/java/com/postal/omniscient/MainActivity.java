d920477ce1d40bea243f3e1f0f686f7f8a92067a
powowstal <powowstal@gmail.com>
2016-04-18 23:58:10 +0300

    some change

---
+++ app/src/main/java/com/postal/omniscient/MainActivity.java
@@ -0,0 +1,51 @@
+package com.postal.omniscient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.postal.omniscient.postal.contact.reader.ReadContacts;
+
+public class MainActivity extends AppCompatActivity {
+    private static String Msg = "MyMsg1";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getContacts();//in test
+
+    }
+    private void getContacts(){
+        ReadContacts getPhones = new ReadContacts();
+        getPhones.setContentResolver(getContentResolver());
+        String [][] phone_contacts = getPhones.readContacts();
+
+   //     for(String n[] : getPhones.readContacts()){
+
+            for (int i=0; getPhones.getX()>=i; i++) {
+                if(phone_contacts[i][0]!=null) {
+                    Log.i(Msg, " ОТВЕТ размер1 - " + phone_contacts[i][0]);
+
+                }
+
+                for (int k=1; getPhones.getY()>k; k++) {
+                    if (phone_contacts[i][k] != null) {
+                        Log.i(Msg, " ОТВЕТ размер2  - " + phone_contacts[i][k]);
+                    }
+                }
+
+                }
+            }
+          //  for(String s : n) {
+            //    if(s!=null) {
+              //      Log.i(Msg, " ОТВЕТ размер2 - " + s);
+                //}
+          //  }
+        //}
+//        Log.i(Msg1," ОТВЕТ размер -"+phone_contacts[5][4]);
+
+
+
+    //}
+
+}
