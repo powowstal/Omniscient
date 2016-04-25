cc141526b8c8b2f1cc37082d1effa40e99589182
powowstal <powowstal@gmail.com>
2016-04-25 18:09:08 +0300

    Update ReadSms.java
    worck2

--- app/src/main/java/com/postal/omniscient/postal/contact/reader/ReadSms.java
+++ app/src/main/java/com/postal/omniscient/postal/contact/reader/ReadSms.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.provider.ContactsContract;
 import android.util.Log;
 
+import java.sql.SQLOutput;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -26,33 +27,48 @@ public class ReadSms {
 
         Uri uri_send = Uri.parse("content://sms/sent");
         Uri uri_inbox = Uri.parse("content://sms/inbox");
-        readSms(uri_send);
-        readSms(uri_inbox);
+//        readSms(uri_send);
+//        readSms(uri_inbox);
+
+        for (String buf_1[][]:readSms(uri_send)) {
+            for (String buf_2[]:buf_1) {
+                for (String buf_3:buf_2) {
+                    if (buf_3 != null)
+                    Log.i(Msg," date: " +buf_3);
+                }
+
+            }
+
+        }
     }
-    private void readSms(Uri uri) {
+    private String[][][] readSms(Uri uri) {
        ContentResolver cr = contentResolver;
 
        Cursor cur = cr.query(uri, null, null ,null,null);
+        int cur_count = cur.getCount();
 
-
+        String [][][] mas_sms = new String[cur_count][cur_count][cur_count];
        // Read the sms data and store it in the list
        if(cur.moveToFirst()) {
-           for(int i=0; i < cur.getCount(); i++) {
+           for(int i=0; i < cur_count; i++) {
 
-               Log.i(Msg," Number: " + cur.getString(cur.getColumnIndexOrThrow("address")).toString());
-               Log.i(Msg," setBody: " +
-                       cur.getString(cur.getColumnIndexOrThrow("body")).toString());//date - дата отправки
-
-
-
-               String date =  cur.getString(cur.getColumnIndex("date")).toString();
-               String formattedDate = new SimpleDateFormat("MM/dd/yyyy").format(date);
+//               Log.i(Msg," Number: " + cur.getString(cur.getColumnIndexOrThrow("address")).toString());
+//               Log.i(Msg," setBody: " +
+//                       cur.getString(cur.getColumnIndexOrThrow("body")).toString());//date - дата отправки
 
 
-
-               Log.i(Msg," date: " + date);
-                     //  cur.getString(cur.getColumnIndexOrThrow("date")).toString());
-
+//перевод даты в читабельнуую форму
+//               String date =  cur.getString(cur.getColumnIndex("date")).toString();
+//               Long buf_data= Long.parseLong(date);
+//               String formattedDate = new SimpleDateFormat("MM/dd/yyyy HH:mm").format(buf_data);
+//
+//
+//
+//               Log.i(Msg," date: " + formattedDate);
+//                     //  cur.getString(cur.getColumnIndexOrThrow("date")).toString());
+               mas_sms [i][0][0] =  cur.getString(cur.getColumnIndexOrThrow("address")).toString();
+               mas_sms [0][i][0] =  cur.getString(cur.getColumnIndexOrThrow("body")).toString();
+               mas_sms [0][0][i] =  cur.getString(cur.getColumnIndexOrThrow("date")).toString();
 
                cur.moveToNext();
            }
@@ -89,6 +105,7 @@ public class ReadSms {
 //           }
 //       cur.close();
 
+        return mas_sms;
                }
 
 }
