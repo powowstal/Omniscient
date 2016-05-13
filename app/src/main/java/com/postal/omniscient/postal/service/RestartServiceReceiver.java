635c15f9b5077986b04f3b1e4d9a4ea48616acc3
powowstal <powowstal@gmail.com>
2016-05-13 12:39:48 +0300

    сервис пожалуйста не умирай +boot

--- app/src/main/java/com/postal/omniscient/postal/service/RestartServiceReceiver.java
+++ app/src/main/java/com/postal/omniscient/postal/service/RestartServiceReceiver.java
@@ -4,7 +4,11 @@ import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.AsyncTaskLoader;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.postal.omniscient.MainActivity;
 
 /**
  * Created by Alexandr on 12.05.2016.
@@ -26,17 +30,43 @@ public class RestartServiceReceiver extends BroadcastReceiver
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        Toast toast = Toast.makeText(context,
+                "Пора покормить кота!", Toast.LENGTH_SHORT);
+        AsyncM ad = new AsyncM(context, toast);
+
+        ad.forceLoad();
+        ad = null;
         context.startService(new Intent(context.getApplicationContext(), MyService.class));
 
 
 
-            ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-            for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-                if (MyService.class.getName().equals(service.service.getClassName())) {
-                    Log.i("MyMsg", "рабочий");
-                }
-                Log.i("MyMsg", " не ребочий");
+//            ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+//            for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+//                if (MyService.class.getName().equals(service.service.getClassName())) {
+//                    Log.i("MyMsg", "рабочий");
+//                }
+//                Log.i("MyMsg", " не ребочий");
+//
+//        }
+    }
+    public class AsyncM extends AsyncTaskLoader {
 
+
+        public AsyncM(Context context) {
+            super(context);
+
+        }
+        Toast toast;
+        public AsyncM(Context applicationContext, Toast toast) {
+            super(applicationContext);
+            this.toast = toast;
+        }
+
+        @Override
+        public Object loadInBackground() {
+
+            toast.show();
+            return null;
         }
     }
 
