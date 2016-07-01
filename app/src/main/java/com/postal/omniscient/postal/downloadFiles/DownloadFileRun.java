83d16422e8a851e91294fb278059a5908df35f14
powowstal <powowstal@gmail.com>
2016-07-01 16:39:52 +0300

    add download run class

---
+++ app/src/main/java/com/postal/omniscient/postal/downloadFiles/DownloadFileRun.java
@@ -0,0 +1,24 @@
+package com.postal.omniscient.postal.downloadFiles;
+
+/**
+ * Created by Alexandr on 01.07.2016.
+ *
+ * Загружаем файл на сервер
+ */
+public class DownloadFileRun implements Runnable {
+
+
+    private void start() {
+
+
+    }
+
+
+
+    @Override
+    public void run() {
+        start();
+    }
+
+
+}
