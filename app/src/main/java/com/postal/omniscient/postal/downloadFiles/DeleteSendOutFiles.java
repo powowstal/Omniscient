9a0f7d3c4aef66d88200061ecc2436d538bf7ef8
powowstal <powowstal@gmail.com>
2016-08-01 18:18:04 +0300

    Update DeleteSendOutFiles.java

--- app/src/main/java/com/postal/omniscient/postal/downloadFiles/DeleteSendOutFiles.java
+++ app/src/main/java/com/postal/omniscient/postal/downloadFiles/DeleteSendOutFiles.java
@@ -8,13 +8,13 @@ import java.io.File;
  * Created by Александр on 10.07.2016.
  */
 public class DeleteSendOutFiles implements Runnable {
-    private File[] allFoldersFiles;
-    private String Msg = "MyMsg";
+       private String Msg = "MyMsg";
     private String isLoaded = "isLoaded ";
     private String feleNAme;
+    private Context context;
 
-    public DeleteSendOutFiles(File[] allFoldersFiles) {
-        this.allFoldersFiles = allFoldersFiles;
+    public DeleteSendOutFiles(Context context) {
+        this.context = context;
     }
     public void setFeleNAme(String feleNAme) {
         this.feleNAme = feleNAme;
@@ -24,25 +24,13 @@ public class DeleteSendOutFiles implements Runnable {
     private void deleteFileIsLoaded(String line) {
 
         try {
-            if (allFoldersFiles != null) {
-                File f2;
-                File[] files;
-                for (File directory : allFoldersFiles) {
-                    if (directory.isDirectory()) {
-                        f2 = new File(directory.toString());
-                        files = f2.listFiles();
-                        for (File inFiles_in : files) {
-                            if (inFiles_in.isFile()) {
-                                Log.i(Msg, "is file " + inFiles_in.toString());
-                                if (line.equals(inFiles_in.getName().toString()))
-                                    new File(inFiles_in.toString()).delete();
-                                Log.i(Msg, "DELETE "+inFiles_in.toString());
-                            }
-                        }
-                    }
-                }
-
+            File fileDir = new File(context.getFilesDir(), "/Omniscient/"+line);
+            if (fileDir.exists()) {
+                fileDir.delete();
+                Log.i(Msg, " DELETE  "+fileDir.toString());
             }
+
+
 
         } catch (Exception e){}
 
